a3463784c42f56e8b2170032251aad8f757394cd
EvgeniBR <genib1989@gmail.com>
2021-11-06 23:16:32 +0200

    axios dependency

--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -3,14 +3,18 @@ import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+  const [term, setTerm] = React.useState('');
 
-  const [term , setTerm,] = React.useState('');
-
-  return <View>
-    <SearchBar onTermSubmit={()=>console.log('term submitted')} term={term} onTermChange = {(newTerm)=> setTerm(newTerm)} />
-    <Text>{term}</Text>
-    
-  </View>
+  return (
+    <View>
+      <SearchBar
+        onTermSubmit={() => console.log('term submitted')}
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+      />
+      <Text>{term}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
