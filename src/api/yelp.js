165df844814d0e4ddc2d1f2e8a9dca8fb958c86a
EvgeniBR <genib1989@gmail.com>
2021-11-07 00:05:01 +0200

    error handling

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://api.yelp.com/v3/buisnesses',
+  baseURL: 'https://api.yelp.com/v3/businesses',
   headers:{
-    Authorization: 'Bearer _5jx8XhBgv_4EP7q3F0pEM_VrUwWiToDAGN_IBcDlZf5grsGTsNPE_xTH_874OziXYmu9zuyVKOb-II4FciVCIDgvmnp5rkytPZ8wYSI4ppKEM1mjJs2K-hjTFOGYXYx' 
+    Authorization: 'Bearer GGkD0dXSnWqcActM9MOQJlkk3KIqleW8upReWvr564_xFdkKh37Xumr4U5LNmMsdkvOdy7oSOddQLwjCPb2zEPSUMYis7bVNP2uP2a2-wBCHhaxU3tmmfDG4M_eGYXYx' 
   }
 })
 
--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = React.useState('');
+  const [results, setResults] = React.useState([]);
+  const [errorMessage, setErrorMessage] = React.useState('');
+  const [isLoading, setIsLoading] = React.useState(false);
+
+
+  const searchApi = async () => {
+    try{
+    setIsLoading(true)
+    const response = await yelp.get('/search',{
+      params: {
+        limit: 50,
+        term,
+        location: 'san jose'
+      }
+    });
+    setResults(response.data.businesses);
+    setIsLoading(false)
+  }catch(err){
+    setErrorMessage('somthing went wrong!')
+  }
+  }
 
   return (
     <View>
       <SearchBar
-        onTermSubmit={() => console.log('term submitted')}
+        onTermSubmit={() => searchApi()}
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)}
       />
-      <Text>{term}</Text>
+      {isLoading?<Text>Loading ... </Text>:null}
+      {errorMessage?<Text>{errorMessage}</Text>:null}
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
