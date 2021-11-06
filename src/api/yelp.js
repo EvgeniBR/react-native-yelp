4492d22e347e12b2f0848045b367d9c37a89ffc2
EvgeniBR <genib1989@gmail.com>
2021-11-06 23:30:52 +0200

    api setup

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,2 +1,9 @@
 import axios from 'axios';
 
+export default axios.create({
+  baseURL: 'https://api.yelp.com/v3/buisnesses',
+  headers:{
+    Authorization: 'Bearer _5jx8XhBgv_4EP7q3F0pEM_VrUwWiToDAGN_IBcDlZf5grsGTsNPE_xTH_874OziXYmu9zuyVKOb-II4FciVCIDgvmnp5rkytPZ8wYSI4ppKEM1mjJs2K-hjTFOGYXYx' 
+  }
+})
+
